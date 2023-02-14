5a0ef3b92d94d4cf3ed6f08ebf3abd464f2b5dd4
uzair shahid <auzair.shahid@gmail.com>
2023-02-14 17:11:59 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,24 @@
+const endDate = "14 Feb 2023 04:56 PM"
+
+document.getElementById('end_date').innerText = endDate;
+const inputs = document.querySelectorAll('input');
+
+const clock = () => {
+    const end = new Date(endDate);
+    const now = new Date();
+    const diff = (end - now)/1000;
+    if (diff < 0) return;
+    inputs[0].value = Math.floor(diff/3600/24);
+    inputs[1].value = Math.floor(diff/3600) % 24;
+    inputs[2].value = Math.floor(diff/60 % 60);
+    inputs[3].value = Math.floor(diff % 60);
+}
+
+clock();
+
+setInterval(
+    () => {
+        clock()
+    },
+    1000
+)
